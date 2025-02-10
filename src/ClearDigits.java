37970079802f7d0e1c83cca27bdc6457baa15c50
Aditya Gupta <ag58290@gmail.com>
2025-02-11 00:38:34 +0530

    Adding Projects

--- src/ClearDigits.java
+++ src/ClearDigits.java
@@ -1,2 +1,34 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * You are given a string s.
+ *
+ * Your task is to remove all digits by doing this operation repeatedly:
+ *
+ * Delete the first digit and the closest non-digit character to its left.
+ * Return the resulting string after removing all digits.
+ *
+ *Input: s = "cb34"
+ */
 public class ClearDigits {
+
+    private static String removeDigits(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(Character.isDigit(s.charAt(i))){
+                sb.deleteCharAt(sb.length()-1);
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String result = removeDigits("cb34");
+        System.out.println(result);
+    }
 }
